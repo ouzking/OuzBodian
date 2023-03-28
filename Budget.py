8c390564077801da1736032a62410b7137340043
ouzking <ousmanearfangkemobodian@gmail.com>
2023-03-28 12:57:19 +0200

    first commit

---
+++ Budget.py
@@ -0,0 +1,7 @@
+nom = input ("Saississez votre nom")
+prenom = input ("Saississez votre prenom")
+depense = int(input("Donnez la somme dépensé du business"))
+revenu = int(input("Donnez le revenu du business"))
+print (nom, prenom, depense, revenu)
+la_gagnante = revenu - depense
+print (la_gagnante) 
